a4152dbe1127e49e99a7264a0692f784855fbfe1
Myagmarsuren <myagmarsurenborkhuu@gmail.com>
2022-10-02 21:42:38 +0800

    add shadow on images

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -80,6 +80,7 @@ function getCss(theme: string, fontSize: string) {
 
     .logo {
         margin: 0 75px;
+        box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
     }
 
     .spacer {
