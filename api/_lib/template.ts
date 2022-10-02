c7c6bfe0b4086bf796cc56952ed16b4d5c9caa41
Myagmarsuren <myagmarsurenborkhuu@gmail.com>
2022-10-02 21:46:20 +0800

    apply shadow only to not 1st children

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -80,6 +80,9 @@ function getCss(theme: string, fontSize: string) {
 
     .logo {
         margin: 0 75px;
+    }
+
+    .logo:not(:first-child) {
         box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
     }
 
